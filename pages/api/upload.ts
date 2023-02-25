ec18ba02a44216a8b43246ec2b028432174bc432
Fidelis Okeke <padrefidelis111@gmail.com>
2023-02-25 09:51:34 +0100

    reversed back

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,12 +1,17 @@
 import multer from "multer";
 import nc from "next-connect";
-import fs, { createWriteStream } from "fs";
-import { join } from "path";
+import path from "path";
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: "./public/uploads",
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const extension = path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + uniqueSuffix + extension);
+  },
+});
+
 const upload = multer({ storage });
-
-// const upload = multer({ storage });
 
 const handler = nc({
   onError: (err, req, res, next) => {
@@ -19,24 +24,9 @@ const handler = nc({
 })
   .use(upload.single("file"))
   .post(async (req, res) => {
-    const fileBuffer = (req as any).file.buffer;
-    const fileName = (req as any).file.originalname;
-    const filePath = `/uploads/${fileName}`;
-
-    const writeStream = createWriteStream(
-      join(process.cwd(), "public", filePath)
-    );
-    writeStream.write(fileBuffer);
-
-    writeStream.on("error", (err) => {
-      return (res as any).status(500).json({ message: err.message });
-    });
-
-    writeStream.on("finish", () => {
-      return (res as any).status(200).json(filePath);
-    });
-
-    writeStream.end();
+    (res as any)
+      .status(200)
+      .send(JSON.stringify("/uploads/" + (req as any).file.filename));
   });
 
 export default handler;
